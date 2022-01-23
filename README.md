4a9c13c518f6108e8baad60e9445088bafde2a16
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-01-23 13:37:49 +0530

    Improved font

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Theatre-Prose
 An App to virtually view theatre plays or rehearse lines from popular theatre plays
+ <br/>
+
+![logo](logo.png?raw=true)
--- app.py
+++ app.py
@@ -27,7 +27,7 @@ window.resizable(False, False)
 window.geometry('876x540')
 frame1 = Frame(window)
 Label(frame1, image=bg).place(bordermode=INSIDE)
-Label(frame1, fg="black", text="Theatre Pros(e)", font=("roboto", 40)).place(x=250, y=70)
+Label(frame1, bg="red4", fg="white", text="Theatre Pros(e)", font=("times", 40, "bold")).place(x=250, y=70)
 frame1.pack(fill="both", expand=True)
 frame2 = None
 count = 0
@@ -36,12 +36,13 @@ def startPlay():
     global lines, frame2, count
     frame1.destroy()
     frame2 = Frame(window)
-    Label(frame2, image=bg).place(bordermode=INSIDE)
-    Label(frame2, fg="black", text="Theatre Pros(e)", font=("roboto", 40)).place(x=250, y=70)
+    Label(frame2, image=bg).place(bordermode=INSIDE)    
     frame2.pack(fill="both", expand=True)
-    Button(frame2, image=nxtimg, command=nextLine).place(x=800, y=430)
-    lines = get_lines(selectedPlay, selectedPlayer)  
-    Label(frame2, fg="black", text=lines[count]['PlayerLine'], font=("roboto", 20)).place(x=100, y=300)
+    Button(frame2, image=nxtimg, command=nextLine).place(x=820, y=460)
+    lines = get_lines(selectedPlay, selectedPlayer) 
+    Label(frame2, bg="red4", fg="white", text=lines[count]['Play'], font=("Times", 30)).place(x=250, y=75)
+    Label(frame2, fg="black", text=lines[count]['Player'], font=("roboto", 20)).place(x=300, y=300)
+    Label(frame2, fg="black", text='"'+lines[count]['PlayerLine']+'"', font=("roboto", 18)).place(x=300, y=400)
     count = count + 1
 
 def nextLine():
@@ -50,14 +51,17 @@ def nextLine():
     frame2 = Frame(window)
     Label(frame2, image=bg).place(bordermode=INSIDE)
     frame2.pack(fill="both", expand=True)
-    Button(frame2, image=nxtimg, command=nextLine).place(x=800, y=430)
+    Button(frame2, image=nxtimg, command=nextLine).place(x=820, y=460)
     if count < len(lines):
-        Label(frame2, fg="black", text=lines[count]['PlayerLine'], font=("roboto", 20)).place(x=100, y=300)
+        Label(frame2, bg="red2", fg="white", text=lines[count]['Play'], font=("Times", 30, "bold")).place(x=250, y=75)
+        Label(frame2, fg="black", text=lines[count]['Player'], font=("roboto", 20)).place(x=300, y=300)
+        Label(frame2, fg="black", text='"'+lines[count]['PlayerLine']+'"', font=("roboto", 15)).place(x=300, y=400)
         count = count + 1
     else:
         if count == len(lines):
-            Label(frame2, fg="black", text=str(count)+" / "+str(count), font=("roboto", 40)).place(x=250, y=70)
-            Label(frame2, fg="black", text="Standing Ovation!", font=("helvetica", 40)).place(x=250, y=200)
+            Label(frame2, fg="black", text="SCORE", font=("Times", 30)).place(x=250, y=75)
+            Label(frame2, fg="black", text=str(count)+" / "+str(count), font=("roboto", 40)).place(x=250, y=150)
+            Label(frame2, fg="black", text="Standing Ovation!", font=("helvetica", 40)).place(x=250, y=230)
         else:
             playsound("assets/applause.mp3")
             window.destroy()        
@@ -72,6 +76,7 @@ def selectPlay(choice):
     selectedPlay = choice
     if selectedMode == modes[0]:
         displayPlayers()
+    Button(frame1, text="Start Play", image=bimage1, command=startPlay).place(x=365, y=450)
 
 def selectPlayer(choice):
     global selectedPlayer
@@ -83,27 +88,26 @@ def displayPlayers():
     players_value = StringVar(frame1)
     players_value.set("Select a Player")
     players_menu = OptionMenu(frame1, players_value, *players, command=selectPlayer)
-    players_menu.config(width=18, height=1, bg='light blue', font=('Helvetica', 15))
-    players_menu.place(x=310, y=340)
+    players_menu.config(width=18, height=1, bg='royalblue1', fg="white", font=('Helvetica', 18, "bold"))
+    players_menu.place(x=300, y=340)
 
 def displayModes():
     modes_value = StringVar(frame1)
     modes_value.set("Select Mode")
     modes_menu = OptionMenu(frame1, modes_value, *modes, command=selectMode)
-    modes_menu.config(width=15, height=1, bg='light blue', font=('Helvetica', 15))
-    modes_menu.place(x=330, y=220)
+    modes_menu.config(width=15, height=1, bg='royalblue1', fg="white", font=('Helvetica', 18, "bold"))
+    modes_menu.place(x=320, y=220)
 
 def displayPlays():
     plays_value = StringVar(frame1)
     plays_value.set("Select a Play")
     plays_menu = OptionMenu(frame1, plays_value, *plays, command=selectPlay)
-    plays_menu.config(width=20, height=1, bg='light blue', font=('Helvetica', 15))
-    plays_menu.place(x=300, y=280)
+    plays_menu.config(width=20, height=1, bg='royalblue1', fg="white", font=('Helvetica', 18, "bold"))
+    plays_menu.place(x=290, y=280)
 
 def main():
     displayModes()
     displayPlays()
-    Button(frame1, text="Start Play", image=bimage1, command=startPlay).place(x=365, y=450)
     window.mainloop()   
 
 
